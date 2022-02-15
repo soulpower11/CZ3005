c1477b2999f0f0e7c0acc15c72bd656b5b55bf1c
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2022-02-15 09:39:09 +0800

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,46 +1,26 @@
-# Adjascency List representation in Python
+#! /usr/bin/env python3
+
+import json
 
 
-class AdjNode:
-    def __init__(self, value):
-        self.vertex = value
-        self.next = None
+# def get_edges(graph):
+#     for d in json.loads(graph):
+#         #node = d['data']
+#         #yield node['source'], node['target']
+#         print(d)
 
 
-class Graph:
-    def __init__(self, num):
-        self.V = num
-        self.graph = [None] * self.V
-
-    # Add edges
-    def add_edge(self, s, d):
-        node = AdjNode(d)
-        node.next = self.graph[s]
-        self.graph[s] = node
-
-        node = AdjNode(s)
-        node.next = self.graph[d]
-        self.graph[d] = node
-
-    # Print the graph
-    def print_agraph(self):
-        for i in range(self.V):
-            print("Vertex " + str(i) + ":", end="")
-            temp = self.graph[i]
-            while temp:
-                print(" -> {}".format(temp.vertex), end="")
-                temp = temp.next
-            print(" \n")
+# def plot(graph, outfile='graph.txt'):
+#     with open(outfile, 'w') as fout:
+#         for src, dst in get_edges(graph):
+#             fout.write('%s  %s\n' % (src, dst))
 
 
-if __name__ == "__main__":
-    V = 5
+if __name__ == '__main__':
+    f = open("G(1).json")
+    data = json.load(f)
+    for i in data:
+         print(data[i])
 
-    # Create graph and edges
-    graph = Graph(V)
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 2)
-    graph.add_edge(0, 3)
-    graph.add_edge(1, 2)
-
-    graph.print_agraph()
+    #plot(f)
+    f.close()
---
+++ graph2.py
@@ -0,0 +1,53 @@
+# Adjascency List representation in Python
+
+import json
+
+class AdjNode:
+    def __init__(self, value):
+        self.vertex = value
+        self.next = None
+
+
+class Graph:
+    def __init__(self, num):
+        self.V = num
+        self.graph = [None] * self.V
+
+    # Add edges
+    def add_edge(self, s, d):
+        node = AdjNode(d)
+        node.next = self.graph[s]
+        self.graph[s] = node
+
+        node = AdjNode(s)
+        node.next = self.graph[d]
+        self.graph[d] = node
+
+    # Print the graph
+    def print_agraph(self):
+        with open('graph.txt', 'w') as fout:
+            for i in range(self.V):
+                fout.write("Vertex " + str(i) + ":")
+                temp = self.graph[i]
+                while temp:
+                    fout.write(" -> {} ".format(temp.vertex))
+                    temp = temp.next
+                fout.write(" \n")
+
+
+if __name__ == "__main__":
+    f = open("G(1).json")
+    data = json.load(f)
+    V = len(data)+1
+    print("V: ", V)
+
+    # Create graph and edges
+    graph = Graph(V)
+
+    for i in data:
+        for d in data[i]:
+            graph.add_edge(int(i) , int(d))
+
+    graph.print_agraph()
+
+    f.close()
